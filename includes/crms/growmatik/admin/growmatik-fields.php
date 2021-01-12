ced2e81c5881b85d0083b789be152dabbc4fd64f
Hector <6560172+hector-codes@users.noreply.github.com>
2021-01-12 14:55:17 +0330

    Revert fields changes

--- includes/crms/growmatik/admin/growmatik-fields.php
+++ includes/crms/growmatik/admin/growmatik-fields.php
@@ -4,45 +4,45 @@ $growmatik_fields = array();
 
 $growmatik_fields['user_email'] = array(
 	'crm_label' => 'Email',
-	'crm_field' => '001',
+	'crm_field' => 'email',
 );
 
 $growmatik_fields['first_name'] = array(
 	'crm_label' => 'First Name',
-	'crm_field' => '003',
+	'crm_field' => 'firstName',
 );
 
 $growmatik_fields['last_name'] = array(
 	'crm_label' => 'Last Name',
-	'crm_field' => '004',
+	'crm_field' => 'lastName',
 );
 
 $growmatik_fields['billing_address_1'] = array(
 	'crm_label' => 'Address',
-	'crm_field' => '005',
+	'crm_field' => 'address',
 );
 
 $growmatik_fields['billing_phone'] = array(
 	'crm_label' => 'Phone Number',
-	'crm_field' => '006',
+	'crm_field' => 'phoneNumber',
 );
 
 $growmatik_fields['phone_number'] = array(
 	'crm_label' => 'Phone Number',
-	'crm_field' => '006',
+	'crm_field' => 'phoneNumber',
 );
 
 $growmatik_fields['billing_country'] = array(
 	'crm_label' => 'Country',
-	'crm_field' => '007',
+	'crm_field' => 'country',
 );
 
 $growmatik_fields['billing_state'] = array(
 	'crm_label' => 'State',
-	'crm_field' => '008',
+	'crm_field' => 'region',
 );
 
 $growmatik_fields['billing_city'] = array(
 	'crm_label' => 'City',
-	'crm_field' => '009',
+	'crm_field' => 'city',
 );
